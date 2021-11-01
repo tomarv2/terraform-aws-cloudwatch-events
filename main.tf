6effc4887115e88c95eb911276a848e2f0f967c8
tomarv2 <varuntomar2019@gmail.com>
2021-11-01 11:01:39 -0700

    enhancing event role

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
 locals {
-  shared_tags = tomap(
-    {
-      "Name"    = "${var.teamid}-${var.prjid}",
-      "team"    = var.teamid,
-      "project" = var.prjid
-    }
-  )
+  default_input = { "sourceVersion" = var.branch, "timeoutInMinutesOverride" = var.timeout }
+  event_input   = var.custom_input == null ? local.default_input : var.custom_input
+}
+
+resource "aws_cloudwatch_event_rule" "event_rule" {
+  count = var.deploy_event_rule ? 1 : 0
+
+  name                = var.name != null ? var.name : "${var.teamid}-${var.prjid}"
+  description         = var.description == null ? "Terraform managed: ${var.teamid}-${var.prjid}" : var.description
+  schedule_expression = var.schedule
+  role_arn            = var.service_role
+
+  tags = merge(local.shared_tags)
+}
+
+resource "aws_cloudwatch_event_target" "event_target_input_type" {
+  count = var.deploy_event_target ? 1 : 0
+
+  target_id = var.name != null ? var.name : "${var.teamid}-${var.prjid}"
+  rule      = aws_cloudwatch_event_rule.event_rule.*.id[count.index]
+  arn       = var.target_arn
+  role_arn  = var.service_role
+  input     = jsonencode(local.event_input)
 }
